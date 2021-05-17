bb8266d0f7fc48aa081b02b2e017396d9ce85257
Mridul2820 <immridulpanda2820@gmail.com>
2021-05-17 11:40:53 +0530

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Header from './components/Header'
 import './styles/app.scss'
 
 const App = () => {
     return (
         <div>
-            TRE
+            <Header />
         </div>
     )
 }
