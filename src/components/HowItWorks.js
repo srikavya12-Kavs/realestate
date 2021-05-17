08680b07af723fcaf263580cac7e3217ed0f94d6
Mridul2820 <immridulpanda2820@gmail.com>
2021-05-17 12:06:21 +0530

    how it works section

---
+++ src/components/HowItWorks.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const HowItWorks = () => {
+    return (
+        <div className="how-it-works">
+            <div className="container">
+                <h2>How it Works</h2>
+                <div className="flex">
+                    <div>
+                        <span className="fas fa-home"></span>
+                        <h4>Find a Property</h4>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+
+                    <div>
+                        <span className="fas fa-dollar-sign"></span>
+                        <h4>Find a Property</h4>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+
+                    <div>
+                        <span className="fas fa-chart-line"></span>
+                        <h4>Find a Property</h4>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HowItWorks
